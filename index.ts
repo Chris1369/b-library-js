848dc4551ff51912c82765dacbb67d2221ba943a
Chris1369 <5928882-Chris1369@users.noreply.gitlab.com>
2022-01-19 11:44:23 +0100

    to ts

---
+++ index.ts
@@ -0,0 +1,12 @@
+function helperUnderTen(num: number): string | number {
+  return num < 10 ? "0" + num : num;
+}
+
+function truncatorText(text: string, maxLength: number = 10): string {
+  if (text.length > maxLength) {
+    return `${text.substring(0, maxLength)}...`;
+  }
+  return text;
+}
+
+export default { helperUnderTen, truncatorText };
